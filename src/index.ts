460bcba728f8b1080e2beddb76790551b1a5df78
Zarman <zarmanwork@gmail.com>
2026-09-04 02:52:31 +0500

    feat: addedd error.html page for s3 bucket.

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,12 @@ Bun.serve({
     if (new URL(request.url).pathname === '/') {
       return new Response(Bun.file('./src/index.html'));
     }
+    if (new URL(request.url).pathname === '/error') {
+      return new Response(Bun.file('./src/error.html'));
+    }
     return new Response('Not Found', { status: 404 });
   },
+
 });
 
 console.log(`Server running on http://localhost:${PORT}`);
